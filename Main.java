e092bc6fa58669ab63ebed5ef37f8152ec937945
Vladimir Levin <94986766+francLv@users.noreply.github.com>
2022-01-25 12:37:33 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String w1 = scanner.next();
+        String w2 = scanner.next();
+        String w3 = scanner.next();
+        System.out.println(w3);
+        System.out.println(w2);
+        System.out.println(w1);
+
+    }
+}
